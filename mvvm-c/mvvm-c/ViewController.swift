c422ca89ef3f94c1655f9f5a2e603ea5e95aa72f
Igor Vaz <igorvaz@me.com>
2020-03-03 22:04:37 -0300

    Blank project

---
+++ mvvm-c/mvvm-c/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-c
+//
+//  Created by Igor Vaz on 03/03/20.
+//  Copyright © 2020 Igor Vaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
